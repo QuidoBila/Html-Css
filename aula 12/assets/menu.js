8198b6df423dccb9849aba447c91a5018c09d73a
QuidoBila <Kidobila007@gmail.com>
2022-11-20 08:00:32 +0000

    date

--- aula 12/assets/menu.js
+++ aula 12/assets/menu.js
@@ -2,7 +2,10 @@
 function mostrar(){
     if(itens.style.display =='block'){
         itens.style.display ='none'
+        document.querySelector('.icon').src = "assets/menu_white.svg"
+
     }else{
         itens.style.display = 'block'
+        document.querySelector('.icon').src = "assets/close_white.svg"
     }
 }
