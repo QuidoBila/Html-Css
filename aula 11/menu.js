569fc2126a2a43e01d32d3c09fc8b795a2a5128f
QuidoBila <Kidobila007@gmail.com>
2022-11-20 06:37:37 +0000

    update

--- aula 11/menu.js
+++
@@ -1,9 +0,0 @@
-
-
-function abrirMenu(){
-    if(itens.style.display == 'block'){
-        itens.style.display = 'none'
-    }else{
-        itens.style.display = 'block'
-    }
-}
---
+++ aula 12/assets/menu.js
@@ -0,0 +1,8 @@
+
+function mostrar(){
+    if(itens.style.display =='block'){
+        itens.style.display ='none'
+    }else{
+        itens.style.display = 'block'
+    }
+}
